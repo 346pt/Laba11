56b74dfc902f9bd81f7316ea042a155479d33ddf
Антисептик <@DESKTOP-KTN6EI8>
2023-04-14 13:09:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    StreamReader text = new StreamReader(@"C:\Users\Антисептик\Desktop\лаба11(10)");
+    string[] massiv = text.ReadToEnd().Split('\n');
+    foreach (var i in massiv)
+    {
+        Console.WriteLine(i);
+    }
+}
+catch (OutOfMemoryException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (System.IO.IOException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
